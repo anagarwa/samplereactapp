77663f6cfb5cb60f4309fd42bcb9c2eec17237ab
anagarwa <anagarwa@adobe.com>
2022-11-30 16:50:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <ReactPlayer url='https://www.youtube.com/watch?v=BQwj6A99oVc' />
+      </div>
   );
 }
-
 export default App;
