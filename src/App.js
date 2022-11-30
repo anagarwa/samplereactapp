bc8607a1ff377d053c2a67e39bcde68bae55cd8a
anagarwa <anagarwa@adobe.com>
2022-11-30 17:36:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ReactPlayer from 'react-player'
 
 function App() {
   return (
-      <div className="App" itemscope itemid="urn:fcsconnection:/document/video" itemtype="urn:fcs:type/video">
+      <div className="App" >
         <ReactPlayer url='https://www.youtube.com/watch?v=O4Dkv4k9aAs' />
       </div>
   );
